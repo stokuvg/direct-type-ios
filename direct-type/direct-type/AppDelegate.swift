aa86395cc2bf12f745e75c608bdceed27df710e9
satoshi-maeda-tud <satoshi.maeda.sw@type.jp>
2020-04-27 15:02:29 +0900

    新規プロジェクトの追加

---
+++ direct-type/direct-type/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  direct-type
+//
+//  Created by ms-mb015 on 2020/04/27.
+//  Copyright © 2020 ms-mb015. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ direct-type/direct-type/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  direct-type
+//
+//  Created by ms-mb015 on 2020/04/27.
+//  Copyright © 2020 ms-mb015. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
