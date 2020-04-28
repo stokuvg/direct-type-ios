6f56392815b9a33d2ac4052902fe3f143a83ef6f
satoshi-maeda-tud <satoshi.maeda.sw@type.jp>
2020-04-28 16:59:30 +0900

    画面一覧を参考にVCを追加

---
+++ direct-type/direct-type/ViewControllers/Career/CareerEditVC.swift
@@ -0,0 +1,30 @@
+//
+//  CareerEditVC.swift
+//  direct-type
+//
+//  Created by ms-mb015 on 2020/04/28.
+//  Copyright © 2020 ms-mb015. All rights reserved.
+//
+
+import UIKit
+
+class CareerEditVC: TmpBasicVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
