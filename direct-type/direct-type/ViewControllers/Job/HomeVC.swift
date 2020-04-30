466fc00b0f0d824e2f8579e37c02fb0d070740ad
satoshi-maeda-tud <satoshi.maeda.sw@type.jp>
2020-04-30 11:18:25 +0900

    レイアウト作成準備

--- direct-type/direct-type/ViewControllers/Job/HomeVC.swift
+++ direct-type/direct-type/ViewControllers/Job/HomeVC.swift
@@ -14,17 +14,7 @@ class HomeVC: TmpNaviTopVC {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.title = "あなたにぴったりの求人"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
