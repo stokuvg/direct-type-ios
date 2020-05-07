f480b891e83a8817058b17f2feac06b84b0cd1b5
satoshi-maeda-tud <satoshi.maeda.sw@type.jp>
2020-05-07 12:40:11 +0900

    未使用部分の削除

--- direct-type/direct-type/ViewControllers/Job/HomeVC.swift
+++ direct-type/direct-type/ViewControllers/Job/HomeVC.swift
@@ -26,7 +26,6 @@ class HomeVC: TmpNaviTopVC {
         homeTableView.rowHeight = 600
         
         homeTableView.registerNib(nibName: "JobOfferBigCardCell", idName: "JobOfferBigCardCell")
-        homeTableView.registerNib(nibName: "KeepCardCell", idName: "KeepCardCell")
         homeTableView.registerNib(nibName: "JobOfferCardMoreCell", idName: "JobOfferCardMoreCell")
         
         self.makeDummyData()
@@ -133,57 +132,6 @@ extension HomeVC: UITableViewDataSource {
             cell.setup(data: data)
             return cell
         }
-        
-        /*
-        // TODO:データ取得を行えるようになった際に変更
-        if row == 0 {
-            let data:[String:Any] = [
-                "row":row,
-                "end":true,
-                "image":"https://type.jp/s/img_banner/top_pc_side_number1.jpg",
-                "job":"PG・SE◆ユーザー直取引多数◆上流工程◆残業月15h◆年間休日128日◆[PG]平均月収25~35万円",
-                "price":"500~700",
-                "special":"850",
-                "area":"東京都23区内",
-                "company":"株式会社キャリアデザインITパートナーズ「type」",
-                "main":"メディアで話題のヘルスケアアプリ運営企業!未経験からWebのお仕事にチャレンジしたい方、歓迎です！",
-            ]
-            
-            cell.setup(data: data)
-        } else if row == 1{
-            let data:[String:Any] = [
-                "row":row,
-                "end":false,
-                "image":"https://type.jp/s/img_banner/top_pc_side_number1.jpg",
-                "job":"PG・SE◆ユーザー直取引多数◆上流工程◆残業月15h◆年間休日128日◆[PG]平均月収25~35万円\nPG・SE◆ユーザー直取引多数◆上流工程◆残業月15h◆年間休日128日◆[PG]平均月収25~35万円",
-                "price":"500~700",
-                "special":"850",
-                "area":"東京都23区内",
-                "company":"株式会社キャリアデザインITパートナーズ「type」",
-                "main":"メディアで話題のヘルスケアアプリ運営企業!未経験からWebのお仕事にチャレンジしたい方、歓迎です！\nメディアで話題のヘルスケアアプリ運営企業!未経験からWebのお仕事にチャレンジしたい方、歓迎です！",
-            ]
-            
-            cell.setup(data: data)
-        } else {
-            let cell = tableView.loadCell(cellName: "KeepCardCell", indexPath: indexPath) as! KeepCardCell
-            let data:[String:Any] = [
-                "row":row,
-                "end":false,
-                "image":"https://type.jp/s/img_banner/top_pc_side_number1.jpg",
-                "job":"PG・SE◆ユーザー直取引多数◆上流工程◆残業月15h◆年間休日128日◆[PG]平均月収25~35万円\nPG・SE◆ユーザー直取引多数◆上流工程◆残業月15h◆年間休日128日◆[PG]平均月収25~35万円",
-                "price":"500~700",
-                "special":"",
-                "area":"東京都23区内",
-                "company":"株式会社キャリアデザインITパートナーズ「type」",
-                "main":"メディアで話題のヘルスケアアプリ運営企業!未経験からWebのお仕事にチャレンジしたい方、歓迎です！\nメディアで話題のヘルスケアアプリ運営企業!未経験からWebのお仕事にチャレンジしたい方、歓迎です！",
-            ]
-            
-            cell.setup(data: data)
-            return cell
-        }
-        
-        return cell
-        */
     }
     
 }
