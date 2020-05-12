03a77e9dc8fc2452af0b0dcbe3175cd1f9f60ecf
satoshi-maeda-tud <satoshi.maeda.sw@type.jp>
2020-05-12 12:18:06 +0900

    無限スクロールの処理の修正 ・スクロールする画像の数を増やして、contentOffsetの位置をスライド時に変更して無限スクロールするように変更 ・それに合わせてページコントロールの現在ページの位置の処理を修正

--- direct-type/direct-type/ViewControllers/Job/detail/JobDetailDataCell.swift
+++ direct-type/direct-type/ViewControllers/Job/detail/JobDetailDataCell.swift
@@ -43,8 +43,6 @@ class JobDetailDataCell: UITableViewCell {
     
     var cellWidth:CGFloat = 0
     
-    
-    var imageList:[String] = []
     var imageCnt:Int = 0
 
     override func awakeFromNib() {
@@ -77,115 +75,58 @@ class JobDetailDataCell: UITableViewCell {
         
         // 画像セット
         let imageUrls:[String] = data["images"] as! [String]
-        Log.selectLog(logLevel: .debug, "imageUrls:\(imageUrls)")
         imageCnt = imageUrls.count
         if imageUrls.count > 1 {
             
             self.mainImagesControl.numberOfPages = imageUrls.count
             
-            for i in 0..<imageUrls.count {
-                let imageUrlString = imageUrls[i]
+            for i in 0..<(imageUrls.count * 3) {
+//            for i in 0..<imageUrls.count {
+                var cnt:Int = 0
+                cnt = i
+                if i >= imageUrls.count {
+                    cnt = i % imageUrls.count
+                }
+                let imageUrlString = imageUrls[cnt]
+//                let imageUrlString = imageUrls[i]
                 let imageUrl = URL(string: imageUrlString)
                 let scrollX:CGFloat = (margin + (viewWidth * CGFloat(i)))
                 let imageView = UIImageView.init(frame: CGRect(x: scrollX, y: imageY, width: imageW, height: imageH))
                 imageView.backgroundColor = UIColor.init(colorType: .color_black)
                 imageView.contentMode = .scaleAspectFit
                 imageView.af_setImage(withURL: imageUrl!)
-                imageView.tag = (i+1)
                 imageView.cornerRadius = 15
-                imageList.append(imageUrlString)
+                
                 self.mainImagesScrollView.addSubview(imageView)
             }
-            self.mainImagesScrollView.contentSize = CGSize(width: (CGFloat(imageUrls.count) * viewWidth), height: viewHeight)
+//            self.mainImagesScrollView.contentSize = CGSize(width: (CGFloat(imageUrls.count) * viewWidth), height: viewHeight)
+            self.mainImagesScrollView.contentSize = CGSize(width: (CGFloat(imageUrls.count*3) * viewWidth), height: viewHeight)
         } else {
             
         }
-    }
-    
-    func setupImageViews() {
-        if imageList.count > 0 {
-            for i in 0..<self.imageList.count {
-                let imageUrlString = imageList[i]
-                let imageUrl = URL(string: imageUrlString)
-                let scrollX:CGFloat = (margin + (viewWidth * CGFloat(i)))
-                let imageView = UIImageView.init(frame: CGRect(x: scrollX, y: imageY, width: imageW, height: imageH))
-                imageView.backgroundColor = UIColor.init(colorType: .color_black)
-                imageView.contentMode = .scaleAspectFit
-                imageView.af_setImage(withURL: imageUrl!)
-                imageView.tag = (i+1)
-                imageView.cornerRadius = 15
-                self.mainImagesScrollView.addSubview(imageView)
-            }
-            
-        }
-    }
-    
-    private func updatePageControl(scrollView:UIScrollView) {
-        Log.selectLog(logLevel: .debug, "JobDetailDataCell updatePageControl start")
-        
-        let pageWidth = scrollView.frame.size.width
-        Log.selectLog(logLevel: .debug, "pageWidth:\(pageWidth)")
-        let pageOffsetX = scrollView.contentOffset.x
-        Log.selectLog(logLevel: .debug, "pageOffsetX:\(pageOffsetX)")
-        let pageNo = Int(pageOffsetX / pageWidth)
-        Log.selectLog(logLevel: .debug, "pageNo:\(pageNo)")
-        self.mainImagesControl.currentPage = pageNo
-        
-        let checkPageNo = Int(pageOffsetX / pageWidth / 1.5)
-        Log.selectLog(logLevel: .debug, "checkPageNo:\(checkPageNo)")
     }
     
 }
 
 extension JobDetailDataCell: UIScrollViewDelegate {
     
-    /*
-    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        Log.selectLog(logLevel: .debug, "JobDetailDataCell scrollViewWillEndDragging start")
-        
-        Log.selectLog(logLevel: .debug, "scrollView:\(scrollView)")
-    }
-    */
-    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
 //        Log.selectLog(logLevel: .debug, "JobDetailDataCell scrollViewDidScroll start")
         
-        let offsetX = self.mainImagesScrollView.contentOffset.x
-//        Log.selectLog(logLevel: .debug, "offsetX:\(offsetX)")
+        let pageWidth = scrollView.contentSize.width / 3.0
         
-        if offsetX > (self.mainImagesScrollView.frame.size.width * 1.5) {
-            // 画像の要素を末尾の要素にする
-            let sortedImage = self.imageList[0]
-            self.imageList.append(sortedImage)
-            // 画像の一覧の先頭要素を削除
-            self.imageList.removeFirst()
-            // 順番が入れ替えられたimageListで描画
-            self.setupImageViews()
-            
-            // ページコントロールを更新
-            self.updatePageControl(scrollView: scrollView)
-            
-//            self.mainImagesScrollView.contentOffset.x -= self.mainImagesScrollView.frame.size.width
-            self.mainImagesScrollView.contentOffset.x -= cellWidth
-            
+        if scrollView.contentOffset.x <= 0.0 || (scrollView.contentOffset.x > pageWidth * 2.0) {
+            scrollView.contentOffset.x = pageWidth
         }
         
-        if offsetX < (self.mainImagesScrollView.frame.size.width * 0.5) {
-            // 画像の末尾要素を先頭の要素にする
-            let sortedImage = self.imageList.last
-            self.imageList.insert(sortedImage!, at: 0)
-            // 画像の一覧の末尾要素を削除
-            self.imageList.removeLast()
-            // 順番が入れ替えられたimageListで描画
-            self.setupImageViews()
-            // ページコントロールを更新
-            self.updatePageControl(scrollView: scrollView)
-            
-//            self.mainImagesScrollView.contentOffset.x += self.mainImagesScrollView.bounds.size.width
-            self.mainImagesScrollView.contentOffset.x += cellWidth
-            
-        }
+        let pageOffsetX = Int(scrollView.contentOffset.x)
         
+        let scrollWidth = Int(scrollView.bounds.size.width)
+        
+        let pageNo = pageOffsetX / scrollWidth
+        let changePageNo = pageNo % imageCnt
+        
+        self.mainImagesControl.currentPage = changePageNo
     }
     
 }
