18ec207e77975ed2dabac952f1c78bfb0e55aae5
satoshi-maeda-tud <satoshi.maeda.sw@type.jp>
2020-06-01 18:00:45 +0900

    H-8 設定TOP画面レイアウト作成

--- direct-type/direct-type/ViewControllers/Setting/SettingVC.swift
+++ direct-type/direct-type/ViewControllers/Setting/SettingVC.swift
@@ -9,22 +9,37 @@
 import UIKit
 
 class SettingVC: TmpBasicVC {
+    
+    @IBOutlet weak var tableView:UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "設定"
+        
+        tableView.backgroundColor = UIColor.init(colorType: .color_white)!
+        tableView.tableFooterView = UIView()
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SettingVC: UITableViewDelegate {
+    
+}
+
+extension SettingVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.contentView.backgroundColor = UIColor.init(colorType: .color_white)
+        return cell
+    }
+    
+    
 }
