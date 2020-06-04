9c0301786ede07f16a2fbda57cc540a5036fab75
yamatakuType <taku.yamada.ys@type.jp>
2020-06-04 15:44:52 +0900

    設問用のセルを作成

---
+++ direct-type/direct-type/ViewControllers/Chemistry/Cell/ChemistrySelectCell.swift
@@ -0,0 +1,15 @@
+//
+//  ChemistrySelectCell.swift
+//  direct-type
+//
+//  Created by yamataku on 2020/06/04.
+//  Copyright © 2020 ms-mb015. All rights reserved.
+//
+
+import UIKit
+
+final class ChemistrySelectCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
