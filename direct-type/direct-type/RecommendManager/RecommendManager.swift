6cdd44242e5bd6fe5dc307683e6ec21d75db81ed
stokuvg <satoshi.tokumitsu.vg@type.jp>
2020-06-16 15:38:57 +0900

    SilverEggレコメンドマネージャ作成

--- direct-type/direct-type/RecommendManager/RecommendManager.swift
+++ direct-type/direct-type/RecommendManager/RecommendManager.swift
@@ -8,55 +8,57 @@
 
 import UIKit
 import PromiseKit
-import RecommendApi
+import SERecommend
 import AWSMobileClient
 
 final public class RecommendManager {
     enum SpecType {
-//        case click
-//        case api111
-//        case ap111
-//        case ap112
-//        case ap211
-        case ap341
-//        case ap511
-//        case ap711
+        case ap111  //▼HOME上部枠：ap111  spec=ap111&num=【要求数】
+        case ap112  //▼HOME下部枠：ap112  spec=ap112&num=【要求数】
+        case ap211  //▼相性診断完了枠：ap211  spec=ap211&num=【要求数】
+        case ap341  //▼案件詳細(トラッキング）：ap341 prod=【閲覧中の案件ID】&spec=ap341&num=0
+        case ap511  //▼応募完了枠：ap511    prod=【応募完了した案件ID1】&spec=ap511&num=【要求数】
+        case ap711  //▼検索結果枠：ap711    spec=ap711&num=【要求数】
         
+        var specID: String {
+            switch self {
+            case .ap111:    return "ap111"
+            case .ap112:    return "ap112"
+            case .ap211:    return "ap211"
+            case .ap341:    return "ap341"
+            case .ap511:    return "ap511"
+            case .ap711:    return "ap711"
+            }
+        }
     }
+    
     public static let shared = RecommendManager()
     private init() {
     }
 }
 extension RecommendManager {
     class func fetchRecommend(type: RecommendManager.SpecType, jobID: String) -> Promise<Void> {
-        let sub: String = AWSMobileClient.default().username ?? ""
+        SERecommendAPI.basePath = Constants.RecommendServer
         switch type {
         case .ap341:
-            return getCareerFetch(jobID: jobID, sub: sub, spec: "ap341")
+            return getRecommendFetch(spec: type.specID, jobID: jobID, num: 0)
+        case .ap511:
+            return getRecommendFetch(spec: type.specID, jobID: jobID)
+        case .ap111, .ap112, .ap211, .ap711:
+            return getRecommendFetch(spec: type.specID)
         }
     }
 }
 
-//AWSMobileClient.default().username
 extension RecommendManager {
-    private class func getCareerFetch(jobID: String, sub: String, spec: String) -> Promise<Void> {
+    private class func getRecommendFetch(spec: String, jobID: String? = nil, num: Int = 100) -> Promise<Void> {
         let (promise, resolver) = Promise<Void>.pending()
-
-
-        print(jobID, sub, spec)
-
-
-        RecomendAPI.dummpycre5ClickGet(prod: jobID, merch: "directtype", cookie: sub)
+        let sub: String = AWSMobileClient.default().username ?? ""
+        RecommendAPI.pycre5JsonRecommendGet(merch: "directtype", cookie: sub, spec: spec, prod: jobID, num: num)
         .done { result in
             resolver.fulfill(Void())
         }
-        .catch { (error) in  //なんか処理するなら分ける。とりあえず、そのまま横流し
-            let myErr: MyErrorDisp = AuthManager.convAnyError(error)
-            print(error.localizedDescription)
-            print(myErr.debugDisp)
-            
-            
-            
+        .catch { (error) in
             resolver.reject(error)
         }
         .finally {
