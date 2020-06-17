e3c02e1b56311a600dd69fda6762a9d8578e144e
yamatakuType <taku.yamada.ys@type.jp>
2020-06-17 16:52:00 +0900

    次へボタンの活性・非活性ロジック周りをリファクタリング

--- direct-type/direct-type/ViewControllers/Setting/AccountChangeCompleteVC.swift
+++ direct-type/direct-type/ViewControllers/Setting/AccountChangeCompleteVC.swift
@@ -15,21 +15,21 @@ enum inputCodeErrorType {
 }
 
 class AccountChangeCompleteVC: TmpBasicVC {
-    @IBOutlet private weak var infomationLabel:UILabel! // 見出し
-    @IBOutlet private weak var textLabel:UILabel!       // 注釈
-    @IBOutlet private weak var inputCodeField:UITextField!
-    @IBOutlet private weak var sendBtn:UIButton!
+    @IBOutlet private weak var infomationLabel: UILabel! // 見出し
+    @IBOutlet private weak var textLabel: UILabel!       // 注釈
+    @IBOutlet private weak var inputCodeField: UITextField!
+    @IBOutlet private weak var sendBtn: UIButton!
     @IBAction private func sendBtnAction() {
         sendCodeAction()
     }
-    @IBOutlet private weak var reSendBtn:UIButton!
+    @IBOutlet private weak var reSendBtn: UIButton!
     @IBAction private func reSendBtnAction() {
         if sendErrorFlag {
             reSendTelPhoneNumber()
         }
     }
     
-    private let maxLength: Int = 6
+    private let codeMaxLength: Int = 6
     private var authCode: Int?
     private var sendErrorFlag = false
 
@@ -57,29 +57,33 @@ private extension AccountChangeCompleteVC {
         inputCodeField.textColor = UIColor(colorType: .color_black)
         inputCodeField.font = UIFont(fontType: .font_M)
         inputCodeField.attributedPlaceholder = NSAttributedString(string: "コードを入力", attributes: [NSAttributedString.Key.foregroundColor : UIColor(colorType: .color_parts_gray)!])
-        NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidChangeSelection(notification:)), name: UITextField.textDidChangeNotification, object: inputCodeField)
+        inputCodeField.addTarget(self, action: #selector(changeButtonState), for: .editingChanged)
         
-        sendBtn.setTitle(text: "次へ", fontType: .font_M, textColor: UIColor.init(colorType: .color_white)!, alignment: .center)
-        reSendBtn.setTitle(text: "電話番号の再送信", fontType: .font_SS, textColor: UIColor.init(colorType: .color_sub)!, alignment: .right)
+        sendBtn.setTitle(text: "次へ", fontType: .font_M, textColor: UIColor(colorType: .color_white)!, alignment: .center)
+        reSendBtn.setTitle(text: "電話番号の再送信", fontType: .font_SS, textColor: UIColor(colorType: .color_sub)!, alignment: .right)
         inputCodeField.becomeFirstResponder()
+        
+        changeButtonState()
+    }
+    
+    var isValidInputText: Bool {
+        guard let inputText = inputCodeField.text, inputCodeField.markedTextRange == nil, inputText.count == codeMaxLength else { return false }
+        return true
     }
     
     @objc
-    func textFieldDidChangeSelection(notification: NSNotification) {
-        let textField = notification.object as! UITextField
-        if let text = textField.text {
-            if textField.markedTextRange == nil && text.count > maxLength {
-                textField.text = text.prefix(maxLength).description
-            }
-        }
+    func changeButtonState() {
+        sendBtn.backgroundColor = UIColor(colorType: isValidInputText ? .color_sub : .color_line)
+        sendBtn.isEnabled = isValidInputText
+        guard let inputText = inputCodeField.text, isValidInputText else { return }
+        inputCodeField.text = inputText.prefix(codeMaxLength).description
     }
     
     // 送信処理
     func sendCodeAction() {
-            
-            var errorType:inputCodeErrorType = .none
+            var errorType: inputCodeErrorType = .none
             // コード番号 チェック
-            if inputCodeField.text?.count != maxLength {
+            if inputCodeField.text?.count != codeMaxLength {
                 // アラート
                 errorType = .few
             } else if inputCodeField.text?.count == 0 {
@@ -96,18 +100,15 @@ private extension AccountChangeCompleteVC {
                     case .few:
                         errorString = "コード番号の桁が違います。"
                 }
-                let sendCodeError = UIAlertController.init(title: "コードエラー", message: errorString, preferredStyle: .alert)
-                let okAction = UIAlertAction.init(title: "OK", style: .default) { (_) in
+                let sendCodeError = UIAlertController(title: "コードエラー", message: errorString, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
                 }
                 sendCodeError.addAction(okAction)
-                
-                self.navigationController?.present(sendCodeError, animated: true, completion: nil)
+                navigationController?.present(sendCodeError, animated: true, completion: nil)
             } else {
                 // TODO:変更認証送信
-                
                 // 設定TOPに戻る
-    //            Log.selectLog(logLevel: .debug, "viewControllers:\(String(describing: self.navigationController?.viewControllers))")
-                self.navigationController?.popToViewController((self.navigationController?.viewControllers[1])!, animated: true)
+                navigationController?.popToViewController((navigationController?.viewControllers[1])!, animated: true)
             }
         }
     
