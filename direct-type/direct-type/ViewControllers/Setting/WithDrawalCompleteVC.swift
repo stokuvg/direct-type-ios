03bc7a404730c460530f2253df49756ec53d3741
yamatakuType <taku.yamada.ys@type.jp>
2020-06-18 15:26:16 +0900

    WithDrawalCompleteVCをリファクタリング

--- direct-type/direct-type/ViewControllers/Setting/WithDrawalCompleteVC.swift
+++ direct-type/direct-type/ViewControllers/Setting/WithDrawalCompleteVC.swift
@@ -9,60 +9,56 @@
 import UIKit
 
 class WithDrawalCompleteVC: TmpBasicVC {
-    
-    @IBOutlet weak var infomationLabel:UILabel!
+    @IBOutlet private weak var infomationLabel:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        self.navigationController?.delegate = self
+        setup()
+    }
+}
 
-        // Do any additional setup after loading the view.
-        self.title = "退会完了"
-        
-        self.infomationLabel.text(text: "退会手続きが完了しました。", fontType: .font_S, textColor: UIColor.init(colorType: .color_black)!, alignment: .center)
+private extension WithDrawalCompleteVC {
+    func setup() {
+        title = "退会完了"
+        infomationLabel.text(text: "退会手続きが完了しました。", fontType: .font_S, textColor: UIColor(colorType: .color_black)!, alignment: .center)
+        setLeftBarButton()
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        if let viewControllers = self.navigationController?.viewControllers {
-            var existSelfInViewControllers = true
-            for viewController in viewControllers {
-                if viewController == self {
-                    existSelfInViewControllers = false
-                    break
-                }
-            }
-            
-            if existSelfInViewControllers {
-                let _appDelegate = UIApplication.shared.delegate as! AppDelegate
-                _appDelegate.displayInitialInputVC()
-                
-            }
-        }
-        super.viewWillAppear(animated)
+    
+    var leftBatButtonFrame: CGRect {
+        return CGRect(x: .zero, y: .zero, width: 30, height: 30)
     }
-
+    func setLeftBarButton() {
+        let leftBarButton = UIButton(type: .system)
+        leftBarButton.frame = leftBatButtonFrame
+        leftBarButton.setTitle(text: "<", fontType: .font_XL, textColor: .white, alignment: .left)
+        leftBarButton.addTarget(self, action: #selector(moveToInitialView), for: .touchUpInside)
+        
+        let backButtonItem =  UIBarButtonItem(customView: leftBarButton)
+        navigationItem.leftBarButtonItem = backButtonItem
+    }
+    
+    @objc
+    func moveToInitialView() {
+        // TODO: 初期画面へ遷移
+        let _appDelegate = UIApplication.shared.delegate as! AppDelegate
+        _appDelegate.displayInitialInputVC()
+    }
 }
 
 extension WithDrawalCompleteVC: UINavigationControllerDelegate {
-    
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         if viewController is WithDrawalCompleteVC {
             Log.selectLog(logLevel: .debug, "この画面と一致")
-            
             let vc = getVC(sbName: "InitialInput", vcName: "InitialInputListVC") as! InitialInputListVC
-            
             let viewControllers = self.navigationController?.viewControllers
-            
-            var newVCs:[UIViewController] = []
+            var newVCs = [UIViewController]()
             newVCs.append(vc)
-            
             for i in 0..<viewControllers!.count {
                 let nowVC = viewControllers![i]
                 newVCs.append(nowVC)
             }
-            
-            self.navigationController?.setViewControllers(newVCs, animated: false)
+            navigationController.setViewControllers(newVCs, animated: false)
         } else {
             Log.selectLog(logLevel: .debug, "この画面以外と一致")
         }
