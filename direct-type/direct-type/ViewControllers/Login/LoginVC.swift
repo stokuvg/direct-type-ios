f93013e20554bf243681644f1bd3d0c884c686bc
yamatakuType <taku.yamada.ys@type.jp>
2020-06-22 11:22:59 +0900

    電話番号入力時のバリデーションと次へボタンの活性・非活性を追加

--- direct-type/direct-type/ViewControllers/Login/LoginVC.swift
+++ direct-type/direct-type/ViewControllers/Login/LoginVC.swift
@@ -7,13 +7,22 @@
 //
 
 import UIKit
+import AWSMobileClient
 
 final class LoginVC: TmpBasicVC {
     @IBOutlet private weak var phoneNumberTextField: UITextField!
+    @IBOutlet private weak var nextButton: UIButton!
     @IBAction private func reasonOfConfirmPhoneButton(_ sender: UIButton) {
+        // TODO: 「電話番号の確認が必要な理由」ボタンタップ時の実装を追加
     }
     @IBAction private func nextButton(_ sender: UIButton) {
+        sendLoginAuthCode()
     }
+    
+    // TODO: パスワードは毎回自動生成する必要があるため、強度の検討が完了した後に自動生成ロジックを実装する
+    // 参照: https://type.qiita.com/y_kawamata/items/e251d8904820d5b5ceaf
+    private let password = "Abcd123$"
+    private let phoneNumberMaxLength: Int = 11
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,5 +34,26 @@ private extension LoginVC {
     func setup() {
         title = "初期入力"
         navigationController?.isNavigationBarHidden = false
+        phoneNumberTextField.addTarget(self, action: #selector(changeButtonState), for: .editingChanged)
+    }
+    
+    func sendLoginAuthCode() {
+//        AWSMobileClient.default().signIn(username: username, password: password)  { (signInResult, error) in
+//            
+//        }
+    }
+    
+    var isValidInputText: Bool {
+        guard let inputText = phoneNumberTextField.text, phoneNumberTextField.markedTextRange == nil,
+            inputText.count == phoneNumberMaxLength else { return false }
+        return true
+    }
+    
+    @objc
+    func changeButtonState() {
+        nextButton.backgroundColor = UIColor(colorType: isValidInputText ? .color_sub : .color_line)
+        nextButton.isEnabled = isValidInputText
+        guard let inputText = phoneNumberTextField.text, isValidInputText else { return }
+        phoneNumberTextField.text = inputText.prefix(phoneNumberMaxLength).description
     }
 }
