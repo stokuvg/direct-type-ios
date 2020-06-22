ebca56285b268332fad9c654de9175009461d691
yamatakuType <taku.yamada.ys@type.jp>
2020-06-22 14:08:11 +0900

    LoginConfirmVCのベースを作成

--- direct-type/direct-type/ViewControllers/Login/LoginConfirmVC.swift
+++ direct-type/direct-type/ViewControllers/Login/LoginConfirmVC.swift
@@ -9,8 +9,39 @@
 import UIKit
 
 final class LoginConfirmVC: TmpBasicVC {
-
+    @IBOutlet private weak var authCodeTextField: UITextField!
+    @IBOutlet private weak var nextButton: UIButton!
+    
+    @IBAction private func resendAuthCodeButton(_ sender: UIButton) {
+    }
+    @IBAction private func nextButton(_ sender: UIButton) {
+    }
+    
+    private let aithCodeMaxLength: Int = 11
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+}
+
+private extension LoginConfirmVC {
+    func setup() {
+        title = "認証コード入力"
+        authCodeTextField.addTarget(self, action: #selector(changeButtonState), for: .editingChanged)
+    }
+    
+    var isValidInputText: Bool {
+        guard let inputText = authCodeTextField.text, authCodeTextField.markedTextRange == nil,
+            inputText.count == aithCodeMaxLength else { return false }
+        return true
+    }
+    
+    @objc
+    func changeButtonState() {
+        nextButton.backgroundColor = UIColor(colorType: isValidInputText ? .color_sub : .color_line)
+        nextButton.isEnabled = isValidInputText
+        guard let inputText = authCodeTextField.text, isValidInputText else { return }
+        authCodeTextField.text = inputText.prefix(aithCodeMaxLength).description
     }
 }
