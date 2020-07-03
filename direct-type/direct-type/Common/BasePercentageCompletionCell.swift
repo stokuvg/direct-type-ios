cfe68162cde6cc8a57315a9e584a70a78b60efce
yamatakuType <taku.yamada.ys@type.jp>
2020-07-03 14:15:06 +0900

    BasePercentageCompletionCellを軽くリファクタ

--- direct-type/direct-type/Common/BasePercentageCompletionCell.swift
+++ direct-type/direct-type/Common/BasePercentageCompletionCell.swift
@@ -8,24 +8,22 @@
 
 import UIKit
 
-protocol BasePercentageCompletionCellDelegate {
+protocol BasePercentageCompletionCellDelegate: class {
     func completionEditAction(sender: BaseTableViewCell)
 }
 
-class BasePercentageCompletionCell: BaseTableViewCell {
-    
-    @IBOutlet weak var stackView:UIStackView!
-    
-    @IBOutlet weak var titleLabel:UILabel!
-    @IBOutlet weak var percentLabel:UILabel!
-    @IBOutlet weak var percentMark:UILabel!
-    @IBOutlet weak var percentProgress:UIProgressView!
-    @IBOutlet weak var editBtn:UIButton!
-    @IBAction func editBtnAction() {
-        self.delegate.completionEditAction(sender: self)
+final class BasePercentageCompletionCell: BaseTableViewCell {
+    @IBOutlet private weak var stackView:UIStackView!
+    @IBOutlet private weak var titleLabel:UILabel!
+    @IBOutlet private weak var percentLabel:UILabel!
+    @IBOutlet private weak var percentMark:UILabel!
+    @IBOutlet private weak var percentProgress:UIProgressView!
+    @IBOutlet private weak var editBtn:UIButton!
+    @IBAction private func editBtnAction() {
+        delegate?.completionEditAction(sender: self)
     }
     
-    var delegate:BasePercentageCompletionCellDelegate!
+    var delegate: BasePercentageCompletionCellDelegate?
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -35,19 +33,16 @@ class BasePercentageCompletionCell: BaseTableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
     
-    func setup(title:String,percent:String) {
-        titleLabel.text(text: title, fontType: .font_Sb, textColor: UIColor.init(colorType: .color_black)!, alignment: .left)
-        percentLabel.text(text: percent, fontType: .font_Sb, textColor: UIColor.init(colorType: .color_sub)!, alignment: .left)
+    func setup(title: String, percent: String) {
+        titleLabel.text(text: title, fontType: .font_Sb, textColor: UIColor(colorType: .color_black)!, alignment: .left)
+        percentLabel.text(text: percent, fontType: .font_Sb, textColor: UIColor(colorType: .color_sub)!, alignment: .left)
         
         let intPercent = Int(percent)
-        
         let percentFloat = Float(intPercent!) / 100
         
         percentProgress.progress = percentFloat
     }
-
 }
