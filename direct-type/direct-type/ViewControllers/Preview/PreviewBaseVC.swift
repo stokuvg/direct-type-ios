360e9ba41a0de393ce58b60af2500778fd350e49
yamatakuType <taku.yamada.ys@type.jp>
2020-07-15 16:55:44 +0900

    PreviewBaseVCを継承しているクラスのTableViewクラスのSelectionを不可にする

--- direct-type/direct-type/ViewControllers/Preview/PreviewBaseVC.swift
+++ direct-type/direct-type/ViewControllers/Preview/PreviewBaseVC.swift
@@ -60,6 +60,7 @@ class PreviewBaseVC: TmpBasicVC {
         //=== テーブル初期化
         self.tableVW.estimatedRowHeight = 100
         self.tableVW.rowHeight = UITableView.automaticDimension
+        self.tableVW.allowsSelection = false
         self.tableVW.register(UINib(nibName: "SpacerTBCell", bundle: nil), forCellReuseIdentifier: "Cell_SpacerTBCell")
         //[C-9系]
         self.tableVW.register(UINib(nibName: "EntryFormInfoTextTBCell", bundle: nil), forCellReuseIdentifier: "Cell_EntryFormInfoTextTBCell")
