79e87f7ede51aae9e1c80cc81b4ec387095ee1f6
yamatakuType <taku.yamada.ys@type.jp>
2020-07-21 15:28:27 +0900

    プロフィールと履歴書のフェッチ処理をBaseTabVCに移管する

--- direct-type/direct-type/Common/BaseTabBC.swift
+++ direct-type/direct-type/Common/BaseTabBC.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class BaseTabBC: UITabBarController {
+    
+    private var profile: MdlProfile?
+    private var resume: MdlResume?
+    private var shouldTransitionToInitialInput: Bool {
+        return profile == nil || resume == nil
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +29,7 @@ class BaseTabBC: UITabBarController {
         // おすすめ     つく  TODO:MVPではタブごと無い
         // 応募管理     つかない
         // マイページ    付かない
+        initialConfigure()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,4 +42,55 @@ class BaseTabBC: UITabBarController {
         super.viewDidAppear(animated)
     }
 
+}
+
+private extension BaseTabBC {
+    func initialConfigure() {
+        if shouldTransitionToInitialInput {
+            fetchProfile()
+        }
+    }
+    
+    // プロフィールデータ取得
+    func fetchProfile() {
+        ApiManager.getProfile(Void(), isRetry: true)
+        .done { result in
+            self.profile = result
+            UserDefaultsManager.shared.setObject(Date(), key: .profileFetchDate)
+        }
+        .catch { _ in }
+        .finally {
+            self.fetchResume()//次の処理の呼び出し
+        }
+    }
+    // 履歴書データ取得
+    func fetchResume() {
+        ApiManager.getResume(Void(), isRetry: true)
+        .done { result in
+            self.resume = result
+        }
+        .catch { _ in}
+        .finally {
+            if self.shouldTransitionToInitialInput {
+                self.showConfirm()
+            }
+        }
+    }
+    
+    func showConfirm() {
+        let alert = UIAlertController(title: "初期入力をしてください", message: "", preferredStyle:  .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { _ in self.transitionToInitialInput() })
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func transitionToInitialInput() {
+        let storyboard = UIStoryboard(name: "Preview", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "Sbid_FirstInputPreviewVC") as! FirstInputPreviewVC
+        vc.hidesBottomBarWhenPushed = true
+        let navi = UINavigationController(rootViewController: vc)
+        navi.modalPresentationStyle = .fullScreen
+        present(navi, animated: true)
+    }
 }
