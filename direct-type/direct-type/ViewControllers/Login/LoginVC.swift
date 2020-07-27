bbce7c713c55e1404d94c4a1b25d78d226a4dc84
yamatakuType <taku.yamada.ys@type.jp>
2020-07-27 14:57:22 +0900

    ログイン画面にもプラポと利用規約ボタンを追加

--- direct-type/direct-type/ViewControllers/Login/LoginVC.swift
+++ direct-type/direct-type/ViewControllers/Login/LoginVC.swift
@@ -13,8 +13,14 @@ import SVProgressHUD
 final class LoginVC: TmpBasicVC {
     @IBOutlet private weak var phoneNumberTextField: UITextField!
     @IBOutlet private weak var nextButton: UIButton!
+    @IBAction func privacyPolicyButton(_ sender: UIButton) {
+        openWebView(type: .privacy)
+    }
+    @IBAction func termsOfServiceButton(_ sender: UIButton) {
+        openWebView(type: .term)
+    }
     @IBAction private func reasonOfConfirmPhoneButton(_ sender: UIButton) {
-        openReasonOfConfirmPhone()
+        openWebView(type: .reasonOfConfirmPhone)
     }
     @IBAction private func nextButton(_ sender: UIButton) {
         trySignIn()
@@ -91,9 +97,9 @@ private extension LoginVC {
         }
     }
     
-    func openReasonOfConfirmPhone() {
+    func openWebView(type: SettingWebType) {
         let vc = getVC(sbName: "Web", vcName: "SettingWebVC") as! SettingWebVC
-        vc.setup(type: .reasonOfConfirmPhone)
+        vc.setup(type: type)
         vc.modalPresentationStyle = .fullScreen
         navigationController?.present(vc, animated: true, completion: nil)
     }
