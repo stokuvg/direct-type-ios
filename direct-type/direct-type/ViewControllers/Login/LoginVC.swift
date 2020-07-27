822b81468745151e252c616c4f121c4044c2d487
stokuvg <satoshi.tokumitsu.vg@type.jp>
2020-07-27 18:10:00 +0900

    #84：新規登録、ログインでの、「電話番号入力」「認証コード入力」で、キーボード表示時にスクロールできるようにした

--- direct-type/direct-type/ViewControllers/Login/LoginVC.swift
+++ direct-type/direct-type/ViewControllers/Login/LoginVC.swift
@@ -11,6 +11,7 @@ import AWSMobileClient
 import SVProgressHUD
 
 final class LoginVC: TmpBasicVC {
+    @IBOutlet private weak var scrollVW: UIScrollView!
     @IBOutlet private weak var phoneNumberTextField: UITextField!
     @IBOutlet private weak var nextButton: UIButton!
     @IBAction func privacyPolicyButton(_ sender: UIButton) {
@@ -36,6 +37,33 @@ final class LoginVC: TmpBasicVC {
         changeButtonState()
         phoneNumberTextField.keyboardType = .numberPad
     }
+    //=== Notification通知の登録 ===
+    // 画面遷移時にも取り除かないもの（他の画面で変更があった場合の更新のため）
+    override func initNotify() {
+    }
+    // この画面に遷移したときに登録するもの
+    override func addNotify() {
+        let nc = NotificationCenter.default
+        nc.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        nc.addObserver(self, selector: #selector(keyboardDidHide(notification:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    // 他の画面に遷移するときに消して良いもの
+    override func removeNotify() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    @objc func keyboardDidShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        if let rect = userInfo["UIKeyboardFrameEndUserInfoKey"] as? CGRect {
+            let safeAreaB = self.view.safeAreaInsets.bottom
+            let szKeyBoard = rect.size
+            scrollVW.contentInset.bottom = szKeyBoard.height + safeAreaB
+        }
+    }
+    @objc func keyboardDidHide(notification: NSNotification) {
+      scrollVW.contentInset.bottom = 0
+    }
+    //=======================================================================================================
 }
 
 private extension LoginVC {
