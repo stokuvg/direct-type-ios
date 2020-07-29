0259d503e84c759adb29531907a7c88d79bb50f6
stokuvg <satoshi.tokumitsu.vg@type.jp>
2020-07-29 11:21:04 +0900

    type応募時の「任意」だった場合には、項目を投げない。（""だった場合にもtype応募時にはnilとして扱う）

--- direct-type/direct-type/AuthManager/ApiManager+Entry.swift
+++ direct-type/direct-type/AuthManager/ApiManager+Entry.swift
@@ -17,73 +17,80 @@ extension WebAPIEntryUserDto {
     }
     init(_ jobCardCode: String, _ profile: MdlProfile, _ resume: MdlResume, _ career: MdlCareer, _ entry: MdlEntry, _ typePassword: String) {
         self.init()
-        self.password = typePassword //入力させたもの
-        self.lastname = profile.familyName
-        self.firstname = profile.firstName
-        self.lastnameKana = profile.familyNameKana
-        self.firstnameKana = profile.firstNameKana
-        self.sex = profile.gender
-        self.email = profile.mailAddress
+        /*[必須]*/self.password = typePassword //入力させたもの
+        /*[必須]*/self.lastname = profile.familyName
+        /*[必須]*/self.firstname = profile.firstName
+        /*[必須]*/self.lastnameKana = profile.familyNameKana
+        /*[必須]*/self.firstnameKana = profile.firstNameKana
+        /*[必須]*/self.sex = profile.gender
+        /*[必須]*/self.email = profile.mailAddress
         //[任意：対応項目なし]self.emailMobile = XXX
-        self.birthday = "\(profile.birthday.dispYmd())+09:00"
+        /*[必須]*/self.birthday = "\(profile.birthday.dispYmd())+09:00"
         if profile.zipCode.count == 7 {
             let zip3: String = String.substr(profile.zipCode, 1, 3)
             let zip4: String = String.substr(profile.zipCode, 4, 4)
-            self.zip = "\(zip3)-\(zip4)"
+            /*[必須]*/self.zip = "\(zip3)-\(zip4)"
         }
-        self.areaValue = profile.prefecture
-        self.address = "\(profile.address1)\(profile.address2)"
+        /*[必須]*/self.areaValue = profile.prefecture
+        /*[必須]*/self.address = "\(profile.address1)\(profile.address2)"
         //[必須（ただしmobileがあればtelは不要）：対応項目なし]self.tel = XXX
-        self.mobile = profile.mobilePhoneNo
-        self.educationValue = resume.educationId //これはFirstInputで入力
-        self.educationName = resume.school.schoolName
-        self.educationDivision = "\(resume.school.faculty)\(resume.school.department)"
+        /*[必須]*/self.mobile = profile.mobilePhoneNo
+        /*[必須]*/self.educationValue = resume.educationId //これはFirstInputで入力
+        /*[必須]*/self.educationName = resume.school.schoolName
+        /*[必須]*/self.educationDivision = "\(resume.school.faculty)\(resume.school.department)"
         let graduationYM: Date = DateHelper.convStrYM2Date(resume.school.graduationYear)
-        self.graduationY = graduationYM.dispYear()
-        self.graduationM = graduationYM.dispMonth() //0埋め不要
-        //self.educationNote = XXX  //example: 学歴細補足です。//新規会員登録フォーム画面では「学歴」
-        self.workStatus = resume.employmentStatus
-        self.changeJobCount = resume.changeCount
+        /*[必須]*/self.graduationY = graduationYM.dispYear()
+        /*[必須]*/self.graduationM = graduationYM.dispMonth() //0埋め不要
+        //[任意：対応項目なし]self.educationNote = XXX  //example: 学歴細補足です。
+        /*[必須]*/self.workStatus = resume.employmentStatus
+        /*[必須]*/self.changeJobCount = resume.changeCount
         /** 経験職種(userOldJob3)を参照 */
         var _userOldJob3List: [UserOldJob3] = []
         _userOldJob3List.append(UserOldJob3(job3Id: resume.lastJobExperiment.jobType, experienceYears: resume.lastJobExperiment.jobExperimentYear))
         for item in resume.jobExperiments {
             _userOldJob3List.append(UserOldJob3(job3Id: item.jobType, experienceYears: item.jobExperimentYear))
         }
-        if _userOldJob3List.count > 0 {
-            self.userOldJob3List = _userOldJob3List
-        }
+        /*[必須]*/self.userOldJob3List = _userOldJob3List
         /** 経験業種(userOldIndustry)を参照 */
         var _userOldIndustryList: [UserOldIndustry] = []
         for item in resume.businessTypes {
             _userOldIndustryList.append(UserOldIndustry(industryId: item))
         }
-        if _userOldIndustryList.count > 0 {
-            self.userOldIndustryList = _userOldIndustryList
-        }
+        /*[必須]*/self.userOldIndustryList = _userOldIndustryList
         if !resume.skillLanguage.languageToeicScore.isEmpty {
-            self.toeic = resume.skillLanguage.languageToeicScore
+            /*[任意]*/self.toeic = resume.skillLanguage.languageToeicScore
         }
         if !resume.skillLanguage.languageToeflScore.isEmpty {
-            self.toefl = resume.skillLanguage.languageToeflScore
+            /*[任意]*/self.toefl = resume.skillLanguage.languageToeflScore
         }
         if !resume.skillLanguage.languageEnglish.isEmpty {
-            self.englishSkillValue = resume.skillLanguage.languageEnglish
+            /*[任意]*/self.englishSkillValue = resume.skillLanguage.languageEnglish
         }
         if !resume.skillLanguage.languageStudySkill.isEmpty {
-            self.otherLanguage = resume.skillLanguage.languageStudySkill
+            /*[任意]*/self.otherLanguage = resume.skillLanguage.languageStudySkill
         }
         //[任意：対応項目なし]self.license = XXX//example: 保有資格あああ
-        self.jobId = jobCardCode
+        /*[必須]*/self.jobId = jobCardCode
         /** 職務経歴書(experienceCompany)を参照 */
         var _experienceCompanyList: [ExperienceCompany] = []
         for item in career.businessTypes {
-            let expCompany = ExperienceCompany(company: item.companyName, startworkY: item.workPeriod.startDate.dispYear(), startworkM: item.workPeriod.startDate.dispMonth(), endworkY: item.workPeriod.endDate.dispYear(), endworkM: item.workPeriod.endDate.dispMonth(), employees: item.employeesCount, employmentId: item.employmentType, salary: item.salary, workNote: item.contents)
+
+            let _employees = item.employeesCount.isEmpty ? nil : item.employeesCount
+            let _employmentId = item.employmentType.isEmpty ? nil : item.employmentType
+            let _salary = item.salary.isEmpty ? nil : item.salary
+            let expCompany = ExperienceCompany(
+                /*[必須]*/company: item.companyName,
+                /*[必須]*/startworkY: item.workPeriod.startDate.dispYear(),
+                /*[必須]*/startworkM: item.workPeriod.startDate.dispMonth(),
+                /*[必須]*/endworkY: item.workPeriod.endDate.dispYear(),
+                /*[必須]*/endworkM: item.workPeriod.endDate.dispMonth(),
+                /*[任意]*/employees: _employees,
+                /*[任意]*/employmentId: _employmentId,
+                /*[任意]*/salary: _salary,
+                /*[必須]*/workNote: item.contents )
             _experienceCompanyList.append(expCompany)
         }
-        if _experienceCompanyList.count > 0 {
-            self.experienceCompanyList = _experienceCompanyList
-        }
+        /*[必須]*/self.experienceCompanyList = _experienceCompanyList
         //[任意：対応項目なし]　スキルシート(userSkill)を参照
         //var _userSkillList: [UserSkill] = []
         //self.userSkillList = _userSkillList
@@ -93,21 +100,21 @@ extension WebAPIEntryUserDto {
             _userLicenseList.append(UserLicense(licenseId: code))
         }
         if _userLicenseList.count > 0 {
-            self.userLicenseList = _userLicenseList
+            /*[任意]*/self.userLicenseList = _userLicenseList
         }
         //[任意：対応項目なし]self.skillsheetFreeword = XXX
         if !entry.ownPR.isEmpty {
-            self.entryEtc = entry.ownPR
+            /*[任意]*/self.entryEtc = entry.ownPR
         }
         var _entryPlaceList: [EntryPlace] = []
         for code in entry.hopeArea {
             _entryPlaceList.append(EntryPlace(placeId: String(code)))
         }
         if _entryPlaceList.count > 0 {
-            self.entryPlaceList = _entryPlaceList
+            /*[任意]*/self.entryPlaceList = _entryPlaceList
         }
         if !entry.hopeSalary.isEmpty {
-            self.salaryId = entry.hopeSalary
+            /*[任意]*/self.salaryId = entry.hopeSalary
         }
         //[任意：対応項目なし] 転職時期マスタより選択
         //public var changeTimeId: String?
@@ -116,13 +123,13 @@ extension WebAPIEntryUserDto {
         //public var entryContactList: [EntryContact]?
         //self.entryContactList = XXX
         if !entry.exAnswer1.isEmpty {
-            self.entryAnswer1 = entry.exAnswer1
+            /*[必須：設問があれば]*/self.entryAnswer1 = entry.exAnswer1
         }
         if !entry.exAnswer2.isEmpty {
-            self.entryAnswer2 = entry.exAnswer2
+            /*[必須：設問があれば]*/self.entryAnswer2 = entry.exAnswer2
         }
         if !entry.exAnswer3.isEmpty {
-            self.entryAnswer3 = entry.exAnswer3
+            /*[必須：設問があれば]*/self.entryAnswer3 = entry.exAnswer3
         }
     }
 }
