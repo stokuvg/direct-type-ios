a4caf318e284358838ccf98a175bc3c91df2b1d3
yamatakuType <taku.yamada.ys@type.jp>
2020-08-05 10:30:41 +0900

    ログインエラーのロジックを修正

--- direct-type/direct-type/ViewControllers/Login/LoginVC.swift
+++ direct-type/direct-type/ViewControllers/Login/LoginVC.swift
@@ -95,12 +95,8 @@ private extension LoginVC {
                         self.trySignIn()
                     }
                 default:
-                    let myError = AuthManager.convAnyError(error)
                     DispatchQueue.main.async {
-                        let errorType = MyErrorDisp.CodeType(rawValue: myError.code)
-                        if errorType == .undefinedUser {
-                            self.showConfirm(title: "認証エラー", message: "電話番号の確認を行ってください。", onlyOK: true)
-                        }
+                        self.showConfirm(title: "認証エラー", message: "電話番号の確認を行ってください。", onlyOK: true)
                         self.changeButtonState()
                         SVProgressHUD.dismiss(); /*Log出力*/LogManager.appendLogProgressErr("[\(NSString(#file).lastPathComponent)] [\(#line): \(#function)]")
                     }
