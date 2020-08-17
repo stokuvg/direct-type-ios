ca95c20ed2427b6e4168588ce01e2860f77636c3
stokuvg <satoshi.tokumitsu.vg@type.jp>
2020-08-17 14:32:41 +0900

    リコメンドで [prod] 先頭保証するため、ファイル生成後にカスタムしたものを上書きしている

--- rebuildAll.sh
+++ rebuildAll.sh
@@ -9,7 +9,9 @@ read -p 'まるっとリビルドします'
 cd api
 sh gen.sh
 cd ../
+## リコメンドで [prod] 先頭保証するため、ファイル生成後にカスタムしたものを上書きしている
+\cp -f ./_Patch/RecommendAPI.swift ./generatedRecommend/promise/SERecommend/Classes/Swaggers/APIs/RecommendAPI.swift
+
+## Pod処理
 cd direct-type
 bundle exec pod install
-
-
