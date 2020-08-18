a56f8a7747f279c1341dee131e6e56165f1b39a8
stokuvg <satoshi.tokumitsu.vg@type.jp>
2020-08-18 16:05:23 +0900

    Badge表示を、0件になったら非表示、追加した場合に1件以上なら表示、その他（削除した結果の1件以上）は表示更新せず現状維持とする

--- direct-type/direct-type/Common/BaseNaviController.swift
+++ direct-type/direct-type/Common/BaseNaviController.swift
@@ -22,16 +22,23 @@ class BaseNaviController: UINavigationController {
     }
     @objc func keepListChanged(notification: NSNotification) {
         //タブの新着チェックは独立させてタブにまかせたい（各所で叩かれるKeep須佐が管理するのは破綻するので）
-        //現在の通知はKeep更新だけど、追加と削除のどちらかも分かるようにした方が良さげ。追加変更だけBadgeつくので
+        //追加だった場合に個数が一位上ならBadgeがつく（現状だと失敗時にもBadgeがついてしまう）
         let cntKeep: Int = KeepManager.shared.getKeepCount()
         if let tabItems: [UITabBarItem] = self.tabBarController?.tabBar.items {
             let tabItem:UITabBarItem = tabItems[1] //キープリストのタブ位置
             tabItem.badgeColor = .clear
             tabItem.setBadgeTextAttributes([NSAttributedString.Key.foregroundColor : UIColor.red], for: .normal)
             if cntKeep > 0 {
-                tabItem.badgeValue = "●"
+                if let keepMode: String = notification.userInfo?[Constants.NotificationKeepStatusChangedParamMode] as? String {
+                    let isKeepAdded = keepMode == Constants.NotificationKeepStatusChangedParamMode_Add
+                    if isKeepAdded {
+                        tabItem.badgeValue = "●"//1件以上あり、キープ追加だった場合にはBadge表示する
+                    } else {
+                        //1件以上あっても、キープ削除の結果だったらBadge状態は変えずに現状維持とする（2件を1件に減らした場合など）
+                    }
+                }
             } else {
-                tabItem.badgeValue = nil
+                tabItem.badgeValue = nil //0件だったらBadge除去
             }
         }
     }
