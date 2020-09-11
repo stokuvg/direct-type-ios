49136d9b911c9977c2101dd12b3ed36556f0378a
satoshi-maeda-tud <satoshi.maeda.sw@type.jp>
2020-09-11 09:23:21 +0900

    キープ一覧のレイアウト修正

--- direct-type/direct-type/ViewControllers/Keep/KeepListVC.swift
+++ direct-type/direct-type/ViewControllers/Keep/KeepListVC.swift
@@ -164,7 +164,7 @@ private extension KeepListVC {
 
 extension KeepListVC: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        Log.selectLog(logLevel: .debug, "KeepListVC heightForRowAt start")
+//        Log.selectLog(logLevel: .debug, "KeepListVC heightForRowAt start")
 //        return DeviceHelper.xsMaxCheck() ? 252 : 232
         
         let spaceWidth:CGFloat = 17.0
@@ -173,7 +173,7 @@ extension KeepListVC: UITableViewDelegate {
         let listTableWidth = self.keepTableView.frame.size.width
         
         let jobWidth = listTableWidth - (spaceWidth * 2) - (stackSpaceWidth * 2)
-        Log.selectLog(logLevel: .debug, "jobWidth:\(jobWidth)")
+//        Log.selectLog(logLevel: .debug, "jobWidth:\(jobWidth)")
         
         var cellHeight:CGFloat
         
@@ -191,9 +191,9 @@ extension KeepListVC: UITableViewDelegate {
         }
 
         if checkTextSize <= jobWidth {
-            cellHeight = 212.0
+            cellHeight = 242.0
         } else {
-            cellHeight = 232.0
+            cellHeight = 262.0
         }
         
         if DeviceHelper.xsMaxCheck() {
